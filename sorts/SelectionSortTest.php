3ba0c40b45706458314ce6de9c9875bebf305bdf
NoMad42 <19962558+NoMad42@users.noreply.github.com>
2023-03-25 14:15:12 +0300

    Небольшой рефакторинг сортировки выбором

--- sorts/SelectionSortTest.php
+++ sorts/SelectionSortTest.php
@@ -1,20 +1,20 @@
 <?php
 
-require_once('SelectionSort.php');
+require_once('selectionSort.php');
 
 staticTest();
 
 function staticTest() {
     $values = [7, 5, 6, 1, 3, 2, 8, 0];
-    SelectionSort($values);
+    selectionSort($values);
     $expectedValues = [0, 1, 2, 3, 5, 6, 7, 8];
     assert(
         $values === $expectedValues,
         new AssertionError(sprintf(
             'Массивы не равны. Ожидали: %s%sПолучили: %s',
-            var_export($values, true),
+            var_export($expectedValues, true),
             PHP_EOL,
-            var_export($expectedValues, true)
+            var_export($values, true),
         ))
     );
 
